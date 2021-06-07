a8e47e96544264d6b5dd010f1e69cf6271b889d7
Dieu Le <18520265@gm.uit.edu.vn>
2021-06-08 02:20:45 +0700

    auto test LSP

--- AutoTestProject/TestLSP.cs
+++ AutoTestProject/TestLSP.cs
@@ -11,41 +11,85 @@ using System.Threading.Tasks;
 namespace AutoTestProject
 {
     public class TestLSP
-    {
-        public string Ten { get; set; }
-        public string DVT { get; set; }
-        public string PTLN { get; set; }
+    {       
+        public class LSP
+        {
+            public string Ten { get; set; }
+            public string DVT { get; set; }
+            public string PTLN { get; set; }
+            public LSP(String ten, String dvt, String ptln)
+            {
+                Ten = ten; DVT = dvt; PTLN = ptln;
+            }
+        }
+        private static  LSP[] sourceTestCase =
+        {
+            new LSP("c1", "Bộ", "1"), 
+           new LSP("c1", "Bộ", "1"),
+           new LSP("c2", "Bộ", "1"),
+            new LSP("c3", "Bộ", "1"),
+           new LSP("c3", "Bộ", "1")
+        };
 
         public IWebDriver driver { get; set; }
-        [SetUp]
+        [OneTimeSetUp]
         public void SetUp()
         {
             driver = new ChromeDriver("D:\\Driver");
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://localhost:44324/Manager/LoaiSP/Index");
+            /*sourceTestCase = new List<LSP>();
+            sourceTestCase.Add(new LSP("b1", "Bộ", "1"));
+            sourceTestCase.Add(new LSP("b1", "Bộ", "1"));*/
         }
         [Test]
-        public void Test1()
-        {
-            codeProcess();
-            //Assert.Pass();
-        }
-        public void codeProcess()
+        [TestCaseSource("sourceTestCase")]
+        /*[TestCase("a1","Bộ","1")]
+        [TestCase("a1", "Bộ", "2")]
+        [TestCase("a2", "Bộ", "3")]
+        [TestCase("a5", "Bộ", "4")]*/
+
+        public void codeProcess(LSP lsp)
         {
             IWebElement tenLoaiSP = driver.FindElement(By.Name("TenLoaiSP"));
-            tenLoaiSP.SendKeys("aaaa1");
-            System.Threading.Thread.Sleep(1000);
+            //tenLoaiSP.SendKeys("aaaa1");
+            tenLoaiSP.Clear();
+            tenLoaiSP.SendKeys(lsp.Ten);
+            System.Threading.Thread.Sleep(500);
+
             IWebElement iWebelement = driver.FindElement(By.Id("MaDVT"));
             SelectElement selected = new SelectElement(iWebelement);
-            selected.SelectByText("Bộ");
-            System.Threading.Thread.Sleep(1000);
+            //selected.SelectByText("Bộ");
+            selected.SelectByText(lsp.DVT);
+            System.Threading.Thread.Sleep(500);
+
             IWebElement loiNhuan = driver.FindElement(By.Name("PhanTramLoiNhuan"));
-            loiNhuan.SendKeys("1");
-            System.Threading.Thread.Sleep(1000);
-            IWebElement btThem = driver.FindElement(By.XPath("(//*[@value='Thêm'])"));
+            //loiNhuan.SendKeys("1");
+            loiNhuan.Clear();
+            loiNhuan.SendKeys(lsp.PTLN);
+            System.Threading.Thread.Sleep(500);
+
+            IWebElement btThem = driver.FindElement(By.XPath("/html/body/section/section/section/main/div/div[1]/div/div[2]/form/input[2]"));
             btThem.Submit();
-            System.Threading.Thread.Sleep(10000);
             
+            IWebElement vali = driver.FindElement(By.XPath("/html/body/section/section/section/main/div/div[1]/div/div[2]/form/div[1]/div[2]/span"));
+            if(vali.Text.Length ==0)
+            {
+                Assert.Pass();
+            }    
+            else
+            {
+                IWebElement dialog = driver.FindElement(By.XPath("/html/body/div/div/div[4]/div/button"));
+                System.Threading.Thread.Sleep(1000);
+                dialog.Click();
+                Assert.Fail();
+                
+            }
+        }
+        [OneTimeTearDown]
+        public void Close()
+        {
+            driver.Close();
         }
     }
 }
